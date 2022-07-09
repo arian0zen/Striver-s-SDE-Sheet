1e3fa6a5e5e594cf058eff7beedda124062eb35c
Arian Shaikh <ariansk90@gmail.com>
2022-07-09 20:58:54 +0530

    Create merge_intervals.py

---
+++ merge_intervals.py
@@ -0,0 +1,22 @@
+from re import A
+from tkinter import N
+
+
+def merge(intervals):
+        intervals.sort(key=lambda x: x[0])
+
+        merged = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+            # otherwise, there is overlap, so we merge the current and previous
+            # intervals.
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
+    
+nums =  [[1,4],[0,2],[3,5]]
+print(merge(nums))
