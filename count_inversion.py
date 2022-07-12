934edea404b3cf902ced83c5948c9165c75873b2
Arian Shaikh <ariansk90@gmail.com>
2022-07-12 17:16:21 +0530

    Update count_inversion.py

--- count_inversion.py
+++ count_inversion.py
@@ -1,26 +1,54 @@
-def mergeSort(arr):
-    if len(arr) == 1:
-        return arr
-    else:
-        a = arr[:len(arr)//2]
-        b = arr[len(arr)//2:]
-        a = mergeSort(a)
-        b = mergeSort(b)
-        c = []
-        i = 0
-        j = 0
-        inversion = 0
-        while i < len(a) and j < len(b):
-            if a[i] < b[j]:
-                c.append(a[i])
-                i = i + 1
-            else:
-                c.append(b[j])
-                inversion += (len(a) - i)
-                j = j + 1
-        c += a[i:]
-        c += b[j:]
-    print (inversion)
-    return c
+'''For a given integer array/list 'ARR' of size 'N' containing all distinct values, find the total number of 'Inversions' that may exist.
+An inversion is defined for a pair of integers in the array/list when the following two conditions are met.
+A pair ('ARR[i]', 'ARR[j]') is said to be an inversion when:
 
-print(mergeSort([5,4,3,2,1]))
+1. 'ARR[i] > 'ARR[j]' 
+2. 'i' < 'j'
+
+Where 'i' and 'j' denote the indices ranging from [0, 'N').'''
+#look at notebook, explanation written there (this one is personal note)
+def mergeSort(arr, n):
+    temp_arr = [0]*n
+    return divide(arr, temp_arr, 0, n-1)
+def divide(arr, temp_arr, left, right):
+    count = 0
+    if left  < right:
+        mid = (left+right)//2
+        count += divide(arr, temp_arr, left, mid)
+        count += divide(arr, temp_arr, mid+1, right)
+            
+        count += conquer(arr, temp_arr, left, mid, right)
+    return count
+def conquer(arr, temp_arr, left, mid, right):
+    i = left
+    j = mid+1
+    k = left
+    count = 0
+    while i <= mid and j <= right:
+        if arr[i] < arr[j]:
+            temp_arr[k] = arr[i]
+            i += 1
+            k += 1
+        else:
+            temp_arr[k] = arr[j]
+            count += (mid-i)+1
+            j += 1
+            k +=1
+    while i <= mid:
+        temp_arr[k] = arr[i]
+        i += 1
+        k += 1
+    while j <= right:
+        temp_arr[k] = arr[j]
+        j += 1
+        k += 1
+        
+    for i in range(left, right+1):
+        arr[i] = temp_arr[i]       
+        
+    return count
+
+                
+
+
+print(mergeSort([5,4,3,2,1,4, 0], 7))
