1c20cbb0208983356cb72816716f536bac819318
Arian Shaikh <ariansk90@gmail.com>
2022-07-14 14:21:11 +0530

    Create 4_sum.py

---
+++ 4_sum.py
@@ -0,0 +1,33 @@
+#i pointer : first number
+#j pointer : second number
+#k pointer : third number
+#end pointer : last number
+#time O(n*^3) space O(n)
+
+def fourSum(nums, target):
+    res = set()
+    n = len(nums)
+    if n == 0:
+        return res
+    nums.sort()
+    for i in range(n):
+            
+        target3 = target - nums[i]
+        for j in range(i+1, n):
+            target2 = target3 - nums[j]
+            k = j+1
+            end = n-1
+            while k < end:
+                sum_now = nums[k] + nums[end]
+                if sum_now < target2:
+                    k += 1
+                elif sum_now > target2:
+                    end -= 1
+                else:
+                    res.add((nums[i],nums[j],nums[k],nums[end]))
+                    k+=1
+    return res
+
+nums = [1,0,-1,0,-2,2]
+target = 0
+print(fourSum(nums, target))
