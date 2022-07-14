9218070de5a9179ff6cb4c3058668e6707d9d775
Arian Shaikh <ariansk90@gmail.com>
2022-07-14 17:30:41 +0530

    Create largest_subarray_with_0_sum.py

---
+++ largest_subarray_with_0_sum.py
@@ -0,0 +1,22 @@
+from asyncio import current_task
+from calendar import c
+
+
+def sumzero(nums):
+    res = []
+    current_sum = nums[0]
+    for i in range(1, len(nums)):
+        current_sum += nums[i]
+        if current_sum > 0:
+            current_sum = 0
+        else:
+            res.append(nums[i])
+            
+    return res   
+        
+        
+        
+    
+nums = [9, -3, 3, -1, 6, -5]
+print(sumzero(nums))
+    
