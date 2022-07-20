61403f2fceb369386edf32156a2ff5f305de346e
Arian Shaikh <ariansk90@gmail.com>
2022-07-20 17:22:42 +0530

    Update intersection_of_two_linked_list.py

--- intersection_of_two_linked_list.py
+++ intersection_of_two_linked_list.py
@@ -41,3 +41,42 @@ class Solution:
             else:
                 head_b = headA
         return head_a
+    
+#also the optimized solution | O(m+n) || O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA, headB):
+        len_a, len_b = 0, 0
+        tempA = headA
+        tempB = headB
+        while tempA:
+            tempA = tempA.next
+            len_a += 1
+        while tempB:
+            tempB = tempB.next
+            len_b += 1
+        
+        skip = abs(len_a-len_b)
+        tempA = headA
+        tempB = headB
+        if len_a > len_b:
+            while skip != 0:
+                tempA = tempA.next
+                skip -= 1
+        else:
+            while skip != 0:
+                tempB = tempB.next
+                skip -= 1
+        while tempA is not None and tempB is not None:
+                if tempA == tempB:
+                    return tempA
+                tempA = tempA.next
+                tempB = tempB.next
+        
+        return None
